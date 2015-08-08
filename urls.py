127c85a2dcefacfb8998536d969d9b285965e0ec
joel ivongbe <engrjoelivon@yahoo.com>
2015-08-08 01:21:41 -0500

    Created an app
    Added the range app to the tuple inside the settings.Generated the
    mappings for the urlpatterns variable.Created a dedicated urls.py to
    serve the mapping for the range app

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rango import urls
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+                       #url(r'^$', "rango.views.home"),
+                       url(r'^rango$', include(urls))
+
 )
